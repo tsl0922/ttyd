ccce42e49aea96cbc45ca95fb4c2611f398d78af
MrLYC <imyikong@gmail.com>
2016-10-31 13:05:25 +0800

    update Dockerfile

---
+++ docker/entry.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [ "${INITSH}" != "" ]; then
+    wget "${INITSH}" -O /root/init/init.sh
+    chmod +x /root/init/init.sh
+    /root/init/init.sh
+fi
+
+ttyd --credential "${USER}":"${PASSWORD}"
