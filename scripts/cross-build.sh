94dd4a0de4f73f0eedd41c3fb21b806d726cc733
Shuanglei Tao <tsl0922@gmail.com>
2020-08-29 13:25:39 +0800

    scripts: download cross toolchain with https

--- scripts/cross-build.sh
+++ scripts/cross-build.sh
@@ -144,7 +144,7 @@ build() {
     echo "=== Installing toolchain ${ALIAS} (${TARGET})..."
 
     mkdir -p "${CROSS_ROOT}" && export PATH="${PATH}:/opt/cross/bin"
-    curl -sLo- "http://musl.cc/${TARGET}-cross.tgz" | tar xz -C "${CROSS_ROOT}" --strip-components 1
+    curl -sLo- "https://musl.cc/${TARGET}-cross.tgz" | tar xz -C "${CROSS_ROOT}" --strip-components 1
 
     echo "=== Building target ${ALIAS} (${TARGET})..."
 
